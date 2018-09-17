4fcc5a7d7ddcbe3f597ba5481c11652d171a9bd6
nitesh-me <43075695+nitesh-me@users.noreply.github.com>
2018-09-17 22:21:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// fun with javascrit
+
+var recipeName="Creamy squash linguine";
+var preparationTime =45;
+var recipeBy="Jimmy Cutter";
+
+var isVegetarian = true;
+var calorie= 223.7;
+
+console.log('Recipe details');
+console.log('--------------------');
+console.log('Recipe Name :' + recipeName);
+console.log('Preparation Time:'+ preparationTime);
+
+console.log('Recipe By:'+ recipeBy);
+
+console.log("is Vegetarian Dish :"+ isVegetarian);
+
+console.log('Calories :'+ calorie );
+// Afer prepearation of the food
+
+console.log('Recipe details after update');
+console.log('--------------------');
+console.log('Recipe Name :' + recipeName);
+console.log('Preparation Time:'+ (preparationTime + 15));
+
+console.log('Recipe By:'+ recipeBy + ' George');
+
+console.log("is Vegetarian Dish :"+ isVegetarian);
+
+console.log('Calories :'+ calorie );
